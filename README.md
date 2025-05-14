a58f653a4b55ea4b2f5379c798fd01faf98752ab
Paulo Vitor Garcia <61211511+paulog22@users.noreply.github.com>
2025-05-14 06:58:31 +0000

    Added `lint:check` and `lint:fix` script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my-tab-news
+
 Repositório com o intuito de ampliar conhecimentos através da implementação do tabnews (de fora a fora), do zero.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>"O único homem que está isento de erros é aquele que não arrisca acertar." - Albert Einstein</h1>
+  return (
+    <h1>
+      "O único homem que está isento de erros é aquele que não arrisca acertar."
+      - Albert Einstein
+    </h1>
+  );
 }
 
 export default Home;
